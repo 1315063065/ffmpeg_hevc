81ac232829e48083c0e38d228e85254fe01760e2
1315063065 <1315063065@qq.com>
2021-09-29 12:07:57 +0800

    aa

--- promise.js
+++ promise.js
@@ -1,43 +1,66 @@
-let packet = beamcoder.packet({ data: Buffer.from(message.data) });
-dec.decode(packet).then((decResult) => {
-  // if (decResult.frames.length === 0) // Frame may be buffered, so flush it out
-  //   decResult = await dec.flush();
+const beamcoder = require('beamcoder');
+const fs = require('fs');
+async function run() {
+  let demuxer = await beamcoder.demuxer('file:./1.mp4'); // Create a demuxer for a file
+
+  // let decoder = beamcoder.decoder({ name: 'h264' }); // Codec asserted. Can pass in demuxer.
+  let videoParams = demuxer.streams.find(x => x.codecpar.codec_type === 'video').codecpar;
+  // demuxer.streams.map((x) => {
+  //   console.log(x.index, x.type, x.codecpar.codec_type)
+  // })
+  // console.log(demuxer.streams)
+  // Set up a decoder using demuxer stream parameters
+  let decoder = beamcoder.decoder({ params: videoParams, stream_index: 0 });
+  let packet = await demuxer.read();
+  for (let x = 0; x < 1000 && packet != null; x++) {
+    packet = await demuxer.read(); // Read next frame. Note: returns null for EOF,)
+    console.log(x, packet.stream_index)
+    if (packet && packet.stream_index === 0) { // Check demuxer to find index of video stream
+
+      let decResult = await decoder.decode(packet);
+      // Do something with the frame data
+      // console.log('flush', decResult);
+
+    }
+  }
+  // for (; packet.stream_index !== 0; packet = await demuxer.read());
+
+  let decResult = await decoder.flush();
+  // let decResult = await decoder.flush(); // Must tell the decoder when we are done
+
   let enc = beamcoder.encoder({ // Create an encoder for JPEG data
     name: 'mjpeg', // FFmpeg does not have an encoder called 'jpeg'
-    width: dec.width,
-    height: dec.height,
-    pix_fmt: dec.pix_fmt.indexOf('422') >= 0 ? 'yuvj422p' : 'yuvj420p',
+    width: decoder.width,
+    height: decoder.height,
+    pix_fmt: decoder.pix_fmt.indexOf('422') >= 0 ? 'yuvj422p' : 'yuvj420p',
     time_base: [1, 1]
-
   });
-  enc.encode(decResult.frames[0]).then(() => {
-    enc.flush().then(() => {
-      let body = jpegResult.packets[0].data;
-      console.log(body)
-      enc.encode(decResult.frames[0]).then((jpegResult) => {
-        let body = jpegResult.packets[0].data;
-        console.log(body)
-      })
+
+
+  for (let i = 0; i < decResult.frames.length; i++) {
+    let jpegResult = await enc.encode(decResult.frames[i]);
+    // console.log('jpegResult.packets[0].data:', jpegResult.packets[0].data)
+    fs.writeFile(`./pic/${i}.jpeg`, jpegResult.packets[0].data, err => {
+      if (err) {
+        console.log(err)
+        return
+      }
     })
-  })
-});
-
-// let jpegResult = await enc.encode(decResult.frames[0]); // Encode the frame
-// await enc.flush(); // Tidy the encoder
+  }
+  await enc.flush(); // Tidy the encoder
 
 
-let packet = beamcoder.packet({ data: Buffer.from(message.data) });
-let decResult = await dec.decode(packet);
-if (decResult.frames.length === 0) // Frame may be buffered, so flush it out
-  decResult = await dec.flush();
-let enc = beamcoder.encoder({ // Create an encoder for JPEG data
-  name: 'mjpeg', // FFmpeg does not have an encoder called 'jpeg'
-  width: dec.width,
-  height: dec.height,
-  pix_fmt: dec.pix_fmt.indexOf('422') >= 0 ? 'yuvj422p' : 'yuvj420p',
-  time_base: [1, 1]
-});
-let jpegResult = await enc.encode(decResult.frames[0]); // Encode the frame
-await enc.flush(); // Tidy the encoder
-let body = jpegResult.packets[0].data;
-console.log(body)
+  // jpegResult.packets.map(x => {
+  //   console.log(x.data)
+  // })
+
+  // fs.writeFile(`./${x}.jpeg`, jpegResult.packets[0].data, err => {
+  //   if (err) {
+  //     console.log(err)
+  //     return
+  //   }
+  // })
+
+}
+
+run();
